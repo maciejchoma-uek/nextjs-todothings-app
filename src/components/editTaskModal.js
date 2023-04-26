86019902adccd8d8e571b132677a7efb37c8b03c
maciejchoma-uek <s218357@student.uek.krakow.pl>
2023-04-26 18:27:20 +0200

    Add edit functionality

--- src/components/editTaskModal.js
+++ src/components/editTaskModal.js
@@ -1,11 +1,34 @@
 import Modal from "react-modal";
-
+import { editTask } from "@/utils/taskManagement";
+import { useEffect, useState } from "react";
 export default function EditTaskModal({
   passedTask,
   isModalOpen,
   handleCloseModal,
   fetchData,
 }) {
+  const [originalData, setOriginalData] = useState({});
+  const [taskName, setTaskName] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+
+  const handleTaskName = (event) => {
+    setTaskName(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleTaskDescription = (event) => {
+    setTaskDescription(event.target.value);
+    console.log(event.target.value);
+  };
+
+  useEffect(() => {
+    if (isModalOpen) {
+      setOriginalData(passedTask);
+      setTaskName(passedTask.taskName);
+      setTaskDescription(passedTask.taskDescription);
+    }
+  }, [isModalOpen]);
+
   return (
     <Modal
       overlayClassName="fixed top-0 left-0 right-0 bottom-0 bg-blue-700 bg-opacity-25 backdrop-blur-sm z-0"
@@ -13,7 +36,30 @@ export default function EditTaskModal({
       isOpen={isModalOpen}
       onRequestClose={handleCloseModal}
     >
-      {passedTask && JSON.stringify(passedTask)}
+      <form>
+        <input
+          onChange={handleTaskName}
+          value={taskName}
+          name="taskName"
+          placeholder="Task name"
+        ></input>
+        <input
+          onChange={handleTaskDescription}
+          value={taskDescription}
+          name="taskDescription"
+          placeholder="Task description"
+        ></input>
+        {passedTask && JSON.stringify(passedTask)}
+        <input
+          onClick={(event) => {
+            event.preventDefault();
+            editTask(originalData, { taskName, taskDescription });
+          }}
+          type="submit"
+          value="edit task"
+        ></input>
+        {/* {error && error} */}
+      </form>
     </Modal>
   );
 }
--- src/utils/taskManagement.js
+++ src/utils/taskManagement.js
@@ -32,3 +32,21 @@ export const deleteTask = async (taskToDelete) => {
 
   await updateDoc(taskRef, { tasks: updatedTasks });
 };
+
+export const editTask = async (taskToEdit, editedTask) => {
+  const taskRef = doc(firestore, "users", auth.currentUser.uid);
+  const docSnapshot = await getDoc(taskRef);
+  if (docSnapshot.exists()) {
+    const tasks = docSnapshot.data().tasks;
+    const indexToUpdate = tasks.findIndex(
+      (task) => task.taskName === taskToEdit.taskName
+    );
+
+    tasks[indexToUpdate].taskName = editedTask.taskName;
+    tasks[indexToUpdate].taskDescription = editedTask.taskDescription;
+
+    await updateDoc(taskRef, { tasks });
+  } else {
+    console.error("Document doesn't exist.");
+  }
+};
