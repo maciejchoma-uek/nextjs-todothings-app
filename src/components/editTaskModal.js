49242e3b7649665df99e81f66fb59649217d1958
maciejchoma-uek <s218357@student.uek.krakow.pl>
2023-04-28 07:25:34 +0200

    Edit and photo modal styling

--- src/components/editTaskModal.js
+++ src/components/editTaskModal.js
@@ -9,6 +9,7 @@ export default function EditTaskModal({
 }) {
   const [originalData, setOriginalData] = useState({});
   const [taskName, setTaskName] = useState("");
+  const [error, setError] = useState("");
   const [taskDescription, setTaskDescription] = useState("");
 
   const handleTaskName = (event) => {
@@ -26,45 +27,69 @@ export default function EditTaskModal({
       setOriginalData(passedTask);
       setTaskName(passedTask.taskName);
       setTaskDescription(passedTask.taskDescription);
+      setError("");
     }
   }, [isModalOpen]);
 
   return (
     <Modal
-      overlayClassName="fixed top-0 left-0 right-0 bottom-0 bg-blue-700 bg-opacity-25 backdrop-blur-sm z-0"
-      className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 width-10/12 z-1 h-4/6 max-h-10/12 bg-blue-950 flex justify-center align-middle"
+      overlayClassName="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-25 backdrop-blur-sm z-0"
+      className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 w-10/12 sm:w-5/12 z-1 h-2/6 rounded-2xl max-h-10/12 bg-neutral-800"
       isOpen={isModalOpen}
       onRequestClose={handleCloseModal}
     >
-      <form>
-        <input
-          onChange={handleTaskName}
-          value={taskName}
-          name="taskName"
-          placeholder="Task name"
-        ></input>
-        <input
-          onChange={handleTaskDescription}
-          value={taskDescription}
-          name="taskDescription"
-          placeholder="Task description"
-        ></input>
-        {passedTask && JSON.stringify(passedTask)}
-        <input
-          onClick={async (event) => {
-            event.preventDefault();
-            try {
-              await editTask(originalData, { taskName, taskDescription });
-            } catch (error) {
-              console.error(error);
-            }
-            handleCloseModal();
-          }}
-          type="submit"
-          value="edit task"
-        ></input>
-        {/* {error && error} */}
-      </form>
+      <div className="flex flex-col h-full justify-center items-center">
+        <h1 className="text-2xl font-bold mb-5">Edit a task</h1>
+        <form className="flex flex-col gap-2 items-center">
+          <label htmlFor="taskName" className="font-bold text-sm text-center">
+            Task Name
+          </label>
+          <input
+            required
+            onChange={handleTaskName}
+            value={taskName}
+            name="taskName"
+            placeholder="Task name"
+            className="!outline-none text-sm placeholder:leading-normal transition-all placeholder:text-xs pb-1.5 placeholder:text-neutral-200 px-3 py-1 rounded-2xl bg-neutral-700 focus:bg-neutral-600 hover:bg-neutral-600"
+          ></input>
+          <label
+            htmlFor="taskDescription"
+            className="font-bold text-sm text-center"
+          >
+            Task Description
+          </label>
+          <input
+            required
+            onChange={handleTaskDescription}
+            value={taskDescription}
+            name="taskDescription"
+            placeholder="Task description"
+            className="!outline-none text-sm placeholder:leading-normal transition-all placeholder:text-xs pb-1.5 placeholder:text-neutral-200 px-3 py-1 rounded-2xl bg-neutral-700 focus:bg-neutral-600 hover:bg-neutral-600"
+          ></input>
+          {/* {passedTask && JSON.stringify(passedTask)} */}
+          {error.length != 0 && (
+            <div className="text-xs font-light text-red-600">{error}</div>
+          )}
+          <input
+            onClick={async (event) => {
+              event.preventDefault();
+              if (taskName.length == 0 || taskDescription.length == 0) {
+                setError("Please fill in both fields.");
+              } else {
+                try {
+                  await editTask(originalData, { taskName, taskDescription });
+                } catch (error) {
+                  console.error(error);
+                }
+                handleCloseModal();
+              }
+            }}
+            type="submit"
+            value="Save"
+            className="text-sm py-1 px-4 m-1 rounded-3xl bg-neutral-700 transition-all hover:bg-neutral-600 cursor-pointer"
+          ></input>
+        </form>
+      </div>
     </Modal>
   );
 }
--- src/components/photoModal.js
+++ src/components/photoModal.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import Modal from "react-modal";
 import { uploadAvatar } from "@/utils/fileUpload";
+import { MdCameraAlt } from "react-icons/md";
 
 export default function PhotoModal({
   isModalOpen,
@@ -76,17 +77,38 @@ export default function PhotoModal({
         handleCloseModal();
         handleCloseCamera();
       }}
-      overlayClassName="fixed top-0 left-0 right-0 bottom-0 bg-blue-700 bg-opacity-25 backdrop-blur-sm z-0"
-      className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 width-10/12 z-1 h-4/6 max-h-10/12 bg-blue-950 flex flex-col justify-center align-middle"
+      overlayClassName="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-25 backdrop-blur-sm z-0"
+      className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 w-10/12 sm:w-5/12 z-1 h-4/6 rounded-2xl max-h-10/12 bg-neutral-800"
     >
-      {content}
-      {!uploadedPhotoUrl ? (
-        <video ref={videoRef} autoPlay />
-      ) : (
-        <img src={uploadedPhotoUrl} />
-      )}
-      <button onClick={handleCapture}>Open camera</button>
-      <button onClick={handleUpload}>Take photo</button>
+      <div className="flex flex-col h-full justify-center items-center">
+        <h1 className="text-2xl font-bold text-center mb-4">
+          Add a profile picture
+        </h1>
+        {!uploadedPhotoUrl ? (
+          <video
+            className="overflow-hidden rounded-2xl border-solid border-2 w-11/12"
+            ref={videoRef}
+            autoPlay
+          />
+        ) : (
+          <img
+            className="overflow-hidden rounded-2xl border-solid border-2 w-11/12"
+            src={uploadedPhotoUrl}
+          />
+        )}
+        <button
+          className="text-sm mt-4 py-1 px-4 m-1 rounded-3xl bg-neutral-700 transition-all hover:bg-neutral-600 cursor-pointer"
+          onClick={handleCapture}
+        >
+          Start the camera
+        </button>
+        <button
+          className="rounded-full w-min h-min p-2 m-2 transition-all bg-neutral-700 hover:bg-neutral-600"
+          onClick={handleUpload}
+        >
+          <MdCameraAlt />
+        </button>
+      </div>
     </Modal>
   );
 }
